40025d887ee5bbe417d6b99af9a98ea06bcebe98
graceduansu <54904766+graceduansu@users.noreply.github.com>
2019-10-05 19:32:39 -0400

    fixed subpages

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def main():
     articles.append(politics_article)
     articles.append(tech_article)
     articles.append(science_article)
-    print(articles)
     return render_template('index.html', articles_list=articles)
 
 
@@ -33,22 +32,38 @@ def get_keyword(article):
 
 @app.route('/tech.html')
 def tech():
-    return render_template('tech.html')
+    articles = []
+    tech_articles = newsapi.get_top_headlines(sources='techcrunch', page_size=20).get('articles')
+    for article in tech_articles:
+        articles.append(article)
+    return render_template('tech.html', articles_list=articles)
 
 
 @app.route('/math.html')
 def math():
-    return render_template('math.html')
+    articles = []
+    math_articles = newsapi.get_everything(q='math', sources='new-scientist').get('articles')
+    for article in math_articles:
+        articles.append(article)
+    return render_template('math.html', articles_list=articles)
 
 
 @app.route('/science.html')
 def science():
-    return render_template('science.html')
+    articles = []
+    science_articles = newsapi.get_top_headlines(sources='new-scientist', page_size=20).get('articles')
+    for article in science_articles:
+        articles.append(article)
+    return render_template('science.html', articles_list=articles)
 
 
 @app.route('/politics.html')
 def politics():
-    return render_template('politics.html')
+    articles = []
+    politics_articles = newsapi.get_everything(q='politics', sources='bbc-news', page_size=20).get('articles')
+    for article in politics_articles:
+        articles.append(article)
+    return render_template('politics.html', articles_list=articles)
 
 
 if __name__ == '__main__':
