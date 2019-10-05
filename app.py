52c9e0b0127e9058fb54692e87ce63f0723f7f8c
catw101 <cwu8959@bths.edu>
2019-10-05 15:35:16 -0400

    update

--- app.py
+++ app.py
@@ -6,12 +6,27 @@ app = Flask(__name__)
 wikipedia.set_lang('simple')
 
 
-@app.route('/', methods=['GET'])
+@app.route('/index.html', methods=['GET'])
 def main():
     page_title = wikipedia.search('tropical storm barry')[0]
     page = wikipedia.page(page_title)
     return render_template('index.html', title=page_title, link=page.url, summary=page.summary)
 
+@app.route('/tech.html')
+def tech():
+	return render_template('tech.html')
+
+@app.route('/math.html')
+def math():
+	return render_template('math.html')
+	
+@app.route('/science.html')
+def science():
+	return render_template('science.html')
+	
+@app.route('/politics.html')
+def politics():
+	return render_template('politics.html')
 
 if __name__ == '__main__':
     app.run()
